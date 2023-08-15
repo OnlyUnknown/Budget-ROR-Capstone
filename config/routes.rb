f8ec47b0b5447f0dd6c87e79c4d7f5bd0ad7671b
OnlyUnknown <abdullahkhrief@gmail.com>
2023-08-15 18:02:56 +0300

    Revert "have added the resources"
    This reverts commit 278738d0db77c295bc7f1af6a533d0cf2770ddc7.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :entities
-  resources :groups
-  resources :categorizations
+
   # Defines the root path route ("/")
-   root "categorizations#index"
+  # root "articles#index"
 end
