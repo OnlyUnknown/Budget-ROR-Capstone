278738d0db77c295bc7f1af6a533d0cf2770ddc7
Abdullah <abdullahkhrief@gmail.com>
2023-08-15 17:45:03 +0300

    have added the resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users
+  resources :entities
+  resources :groups
+  resources :categorizations
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "categorizations#index"
 end
