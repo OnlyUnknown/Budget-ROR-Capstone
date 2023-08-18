6848f09cd044fa1a4f355f5ce4eb94a595183f0a
Abdullah <abdullahkhrief@gmail.com>
2023-08-18 19:11:28 +0300

    have completed the add category page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :entity
   belongs_to :group
+
+  validates :group_id, presence: true
+  validates :entity_id, presence: true
 end
--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,5 @@ class Entity < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :categories
   has_many :groups, through: :categories
+  validates :name, presence: true
 end
